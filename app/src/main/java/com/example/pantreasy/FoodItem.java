af47ef3b3dcf4533c0dd051219cb794114f8721f
Jacob Lopez <jacob_m_lopez@hotmail.com>
2019-04-29 15:11:57 -0700

    added perishable as a variable in FoodItem class

--- app/src/main/java/com/example/pantreasy/FoodItem.java
+++ app/src/main/java/com/example/pantreasy/FoodItem.java
@@ -6,12 +6,14 @@ public class FoodItem {
     public String name;
     public Date expirationDate;
     public String unitOfMeasurement;
+    public boolean perishable;
     float quantity;
 
-    public FoodItem(String name, Date expDate, String unitOfMeasurement, float quanitity) {
+    public FoodItem(String name, Date expDate, String unitOfMeasurement, float quanitity, boolean perishable) {
         this.name = name;
         this.expirationDate = expDate;
         this.unitOfMeasurement= unitOfMeasurement;
         this.quantity = quanitity;
+        this.perishable = perishable;
     }
 }
