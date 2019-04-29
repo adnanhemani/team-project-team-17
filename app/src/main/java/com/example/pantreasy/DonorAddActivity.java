404226ba7087f8c1ba81471a6a2ff272cf802fb9
Jacob Lopez <jacob_m_lopez@hotmail.com>
2019-04-29 15:21:06 -0700

    changed perishable to be checked to start with

--- app/src/main/java/com/example/pantreasy/DonorAddActivity.java
+++ app/src/main/java/com/example/pantreasy/DonorAddActivity.java
@@ -3,7 +3,9 @@ package com.example.pantreasy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,12 +15,36 @@ import java.util.ArrayList;
 
 public class DonorAddActivity extends AppCompatActivity {
     private ArrayList<FoodItem> mFoodList;
+    private Button addToList;
+    private Button clearList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donor_add);
-        ImageView image = findViewById(R.id.main_image);
+        ConstraintLayout layout = findViewById(R.id.donor_add);
+        ImageView foodImage = layout.findViewById(R.id.main_image);
+        addToList = layout.findViewById(R.id.add_to_list_button);
+        clearList = layout.findViewById(R.id.clear_list_button);
+        setOnClickForAddButton();
+        setOnClickForClearButton();
+        foodImage.setClipToOutline(true);
+    }
 
-        image.setClipToOutline(true);
+    private void setOnClickForAddButton() {
+        addToList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void setOnClickForClearButton() {
+        addToList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
