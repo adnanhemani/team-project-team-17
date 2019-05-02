e18ce759c27e12abbf9e2cad57dafb02958d8617
Jacob Lopez <jacob_m_lopez@hotmail.com>
2019-05-02 14:43:21 -0700

    moving to kresge, need to commit

---
+++ app/src/main/java/com/example/pantreasy/DonationItem.java
@@ -0,0 +1,12 @@
+package com.example.pantreasy;
+
+import java.util.ArrayList;
+
+public class DonationItem {
+    public Profile profile;
+    public ArrayList<FoodItem> foodItems;
+    public String comment;
+
+    public DonationItem(Profile profile, ArrayList<FoodItem> foodItems, String comment)
+
+}
--- app/src/main/java/com/example/pantreasy/PantryViewDonationsActivity.java
+++ app/src/main/java/com/example/pantreasy/PantryViewDonationsActivity.java
@@ -2,10 +2,20 @@ package com.example.pantreasy;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class PantryViewDonationsActivity extends AppCompatActivity {
-
+    private ArrayList<DonorResponseItem> mResponseItems;
+    private RecyclerView mRecyclerView;
+    private ConstraintLayout mLayout;
+    private DonorResponseItemAdapter mAdapter;
+    private ImageButton mHomeButton;
+    private ImageButton mConfirmButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
